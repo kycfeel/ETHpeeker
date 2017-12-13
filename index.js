e94b56d76e5fc1fd67c55f980ad836a183004c19
Yechan Kim <kycfeel@gmail.com>
2017-12-13 22:36:50 +0900

    반복 시세 알리미 종료 로직 수정.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let pushInterval;
 
 bot.onText(/\/start/, (msg) => {
 
-bot.sendMessage(msg.chat.id, "빗썸의 실시간 이더리움 시세를 확인할 수 있습니다. 아래 버튼을 눌러주세요. \n\n주의 : 개발자와 애플리케이션은 빗썸과 완전히 무관하며, 애플리케이션 사용으로 발생한 모든 형태의 피해는 사용자에게 있습니다.", {
+bot.sendMessage(msg.chat.id, "빗썸의 실시간 이더리움 시세를 확인할 수 있습니다. 아래 버튼을 눌러주세요. \n\n주의 : 개발자와 애플리케이션은 빗썸과 완전히 무관하며, 애플리케이션 사용으로 발생한 모든 형태의 피해는 사용자에게 있습니다. 종종 유지보수 또는 개인 서버의 문제로 서비스가 중단되거나 느려질 수 있습니다. 너그럽게 이해해 주세요!", {
 "reply_markup": {
     "keyboard": [['이더리움(ETH) 시세 확인'],['반복 시세 알리미', '정지']],
     }
@@ -47,10 +47,10 @@ bot.on('message', (msg) => {
   })
 }
 else if (msg.text === '반복 시세 알리미') {
-  console.log("Telegram Chat ID : " + msg.chat.id);
+  console.log("Telegram Chat ID : " + msg.chat.id + " is now using 반복 시세 알리미!");
   secTrigger = 1;
 
-  bot.sendMessage(msg.chat.id, "5초 간격으로 실시간 이더리움 시세를 알려줍니다. \n\n정지를 누르면 알림을 멈출 수 있습니다.");
+  bot.sendMessage(msg.chat.id, "5초 간격으로 실시간 이더리움 시세를 알려줍니다. \n\n정지를 누르면 알림을 멈출 수 있습니다. \n\n주의 : 가끔 `정지` 버튼을 눌러도 알리미가 멈추지 않는 문제가 발생할 수 있습니다. 이럴 경우 임시로 봇을 차단한 후 다시 추가해 주세요. 빠르게 수정하겠습니다.");
 
 pushInterval = setInterval(()=> {
     request({
@@ -74,13 +74,15 @@ pushInterval = setInterval(()=> {
 }
 
 else if (msg.text === '정지') {
+  console.log(msg.chat.id + ' STOPPED to use 반복 시세 알리미!');
   if (secTrigger) {
-    clearInterval(pushInterval);
     secTrigger = 0;
     bot.sendMessage(msg.chat.id, "반복 시세 알리미가 정지되었습니다.");
+    clearInterval(pushInterval);
   }
   else {
     bot.sendMessage(msg.chat.id, "아직 알리미 기능을 사용하고 있지 않습니다.");
+    clearInterval(pushInterval);
     }
   }
 });
